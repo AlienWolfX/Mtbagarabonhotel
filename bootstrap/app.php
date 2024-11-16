ebc4bf6a10aeca8efa35272a14d842d36f20a67e
Allen Cruiz <cruizallen2@gmail.com>
2024-11-16 09:38:01 +0800

    updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,11 +11,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
-
             'admin' => \App\Http\Middleware\Admin::class,
-
         ]);
 
 
